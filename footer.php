fb1b998cd45fed46a745283180d8680e80e0974a
aasimdev <aasimdev69@gmail.com>
2024-02-08 00:33:50 +0500

    resolved conflicts

--- footer.php
+++ footer.php
@@ -44,6 +44,15 @@ if ($domain === 'localhost' || $domain === '127.0.0.1') {
                 <a href="/contact-us">Contact</a>
             </div>
 
+            <div class="fls-text">
+                <p>Matters discussed in this press release contain forward-looking statements within the meaning of the Private Securities Litigation Reform Act of 1995. When used in this press release, the words "anticipate," "believe," "estimate," "may," "intend," "expect" and similar expressions identify such forward-looking statements. Actual results, performance or achievements could differ materially from those contemplated, expressed or implied by the forward-looking statements contained herein. These forward-looking statements are based largely on the expectations of the Company and are subject to a number of risks and uncertainties. These include, but are not limited to, risks and uncertainties associated with: the impact of economic, competitive and other factors affecting the Company and its operations, markets, the impact on the national and local economies resulting from terrorist actions, the impact of public health epidemics on the global economy and other factors detailed in reports filed by the Company with the United States Securities and Exchange Commission.
+
+                    <br><br>
+
+                    Any forward-looking statement made by us in this press release is based only on information currently available to us and speaks only as of the date on which it is made. We undertake no obligation to publicly update any forward-looking statement, whether written or oral, that may be made from time to time, whether as a result of new information, future developments or otherwise.
+                </p>
+            </div>
+
             <p>© 2024 NewHydrogen. Inc.</p>
         </div>
     </div>
