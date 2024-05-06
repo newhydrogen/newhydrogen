81704d451bf5b88b227562b98aa407f67cbdd191
aasimdev <aasimdev69@gmail.com>
2024-05-06 17:59:40 +0500

    added popup for marketing

--- footer.php
+++ footer.php
@@ -49,7 +49,7 @@ if ($domain === 'localhost' || $domain === '127.0.0.1') {
                 <p>Matters discussed in this page contain forward-looking statements within the meaning of the Private Securities Litigation Reform Act of 1995. When used in this page, the words "anticipate," "believe," "estimate," "may," "intend," "expect" and similar expressions identify such forward-looking statements. Actual results, performance or achievements could differ materially from those contemplated, expressed or implied by the forward-looking statements contained herein. These forward-looking statements are based largely on the expectations of the Company and are subject to a number of risks and uncertainties. These include, but are not limited to, risks and uncertainties associated with: the impact of economic, competitive and other factors affecting the Company and its operations, markets, the impact on the national and local economies resulting from terrorist actions, the impact of public health epidemics on the global economy and other factors detailed in reports filed by the Company with the United States Securities and Exchange Commission.
 
                     <br><br>
- 
+
                     Any forward-looking statement made by us in this page is based only on information currently available to us and speaks only as of the date on which it is made. We undertake no obligation to publicly update any forward-looking statement, whether written or oral, that may be made from time to time, whether as a result of new information, future developments or otherwise.
                 </p>
             </div>
@@ -58,6 +58,35 @@ if ($domain === 'localhost' || $domain === '127.0.0.1') {
         </div>
     </div>
 </footer>
+
+
+<div class="modal invest-modal fade" id="investModal" tabindex="-1" aria-labelledby="investModalLabel" aria-hidden="true" data-bs-backdrop="static">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="jotformForm" action="https://submit.jotform.com/submit/241246709411048/" method="POST">
+                    <h3>Keep up with our progress by subscribing to our email newsletter!</h3>
+                    <div class="mb-3">
+                        <input type="hidden" name="formID" value="241246709411048">
+                        <input type="email" name="q3_email" placeholder="Email" class="form-control">
+                    </div>
+                    <button type="submit">
+                        <span class="invest-sbmit-btn">Enter</span>
+                        <div class="spinner-border text-light" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </button>
+                </form>
+                <div class="alert alert-success invest-alert" role="alert">
+                    It's submitted successfully.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
