03eca184f8562acdde7aaa6fca03b254a18d4d2c
aasimdev <aasimdev69@gmail.com>
2024-07-15 05:50:33 +0500

    added new video and news

--- data/news-data.php
+++ data/news-data.php
@@ -3,6 +3,41 @@
 
 $news = [
     [
+        'id' => "66",
+        'title' => "NewHydrogen’s CEO and Sustainable Farming Expert Explore the Role of Green Hydrogen in Agriculture",
+        'date' => "July 15, 2024",
+        'excerpt' => "NewHydrogen, Inc. (OTCMKTS:NEWH), the developer of ThermoLoop™, a breakthrough technology that uses water and heat rather than electricity to produce the world’s cheapest green hydrogen, today announced a podcast featuring CEO Steve Hill and Dr. Krish Jayachandran with Environmental Microbiology/Soil Science in the Department of Earth and Environment at Florida International University, discussing advancements in green hydrogen production",
+        'description' => [
+            [
+                "<p><strong>Dr. Krish Jayachandran presents green hydrogen: a catalyst for sustainable farming</strong></p>"
+            ],
+            [
+                "<p>SANTA CLARITA, Calif. (July 15, 2024) — NewHydrogen, Inc. (OTCMKTS:NEWH), the developer of ThermoLoop™, a breakthrough technology that uses water and heat rather than electricity to produce the world’s cheapest green hydrogen, today announced a podcast featuring CEO Steve Hill and Dr. Krish Jayachandran with Environmental Microbiology/Soil Science in the Department of Earth and Environment at Florida International University, discussing advancements in green hydrogen production. </p>"
+            ],
+            [
+                "<p>In a recent podcast, Steve Hill and Dr. Jayachandran discussed the transformative potential of green hydrogen in sustainable agriculture. Dr. Jayachandran stated, “This green hydrogen, green ammonia can revolutionize future sustainable agriculture practice if properly implemented.” </p>"
+            ],
+            [
+                "<p>The conversation between Dr. Jayachandran and Hill revolved around the pressing need for collaborative efforts to address research and development challenges in the field. They underscored the ecological advantages and diverse applications of green hydrogen, projecting its use in powering farm machinery and irrigation systems, all while mitigating carbon emissions. </p>"
+            ],
+            [
+                "<p>In his concluding remarks, Dr. Jayachandran asserted, “The future of agriculture hinges on the widespread adoption of green technologies and sustainable practices.”  </p>"
+            ],
+            [
+                "<p>Dr. Krish Jayachandran received his PhD in Plant Pathology with specialization in Soil Microbiology and Soil Science, from Kansas State University. Dr. Jayachandran is a Distinguished University Professor of Environmental Microbiology/Soil Science in the Department of Earth and Environment. Using microbiology as a central tool, he is specialized in nutrient cycling and soil health, microbial diversity and activity, isolation and characterization of pesticide degrading microorganisms, biocontrol of invasive plant species, marine and freshwater toxins and microbial degradation, and soil-plant-microbial interactions. Dr. Jayachandran is listed as Google Scholar at <a href='https://scholar.google.com/citations?user=cikM3poAAAAJ'> https://scholar.google.com/citations?user=cikM3poAAAAJ </a>.</p>"
+            ],
+            [
+                "<p> Watch the full discussion on the NewHydrogen Podcast featuring Dr. Krish Jayachandran at  <a href=\"https://newhydrogen.com/videos/ceo-podcast/dr-krish-jayachandran-florida-international-university\">https://newhydrogen.com/videos/ceo-podcast/dr-krish-jayachandran-florida-international-university</a>.  </p>"
+            ],
+
+            [
+                "<p>For more information about NewHydrogen, please visit <a href=\"https://newhydrogen.com/\">https://newhydrogen.com/</a></p>"
+            ]
+        ],
+        'category' => "pressrelease",
+        'biosolar' => false
+    ],
+    [
         'id' => "65",
         'title' => "NewHydrogen CEO Steve Hill Discusses Training Next Generation of Energy and Climate Tech Entrepreneurs with Executive Director of TEX-E",
         'date' => "July 2, 2024",
