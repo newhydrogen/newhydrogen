5fb9c069c3c4aa2eab213581459668369f9100a3
aasimdev <aasimdev69@gmail.com>
2024-10-19 08:16:01 +0500

    updated header for black page

--- header.php
+++ header.php
@@ -112,7 +112,6 @@
                 $page_name = basename($_SERVER['PHP_SELF']);
                 if ($page_name == "index.php" || $page_name == "" || $page_name == "/") {
                 ?>homeC<?php } ?>
-                <?php if ($page_name == "applications.php") { ?> nav_darkk<?php } ?>
                 <?php if ($page_name == "investors.php") { ?> investPage<?php } ?>
                 <?php if ($page_name == "3reasons.php") { ?> investPage<?php } ?>">
 
