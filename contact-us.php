9c9b17d9c0cdeadc92602b361c8065f7927f591c
aasimdev <aasimdev69@gmail.com>
2024-12-19 02:50:24 +0500

    added validation for the form

--- contact-us.php
+++ contact-us.php
@@ -62,51 +62,49 @@ include('header.php'); ?>
                 <input type="email" class="form-control" placeholder="Email" name="q5_email" id="input_5" required>
 
                 <textarea name="q6_message" id="input_6" cols="30" rows="5" class="form-control" placeholder="Message"></textarea>
-                <!-- <input type="hidden" name="recaptcha_response" id="recaptchaResponse"> -->
+
                 <div class="form-line captcha-field jf-required" data-type="control_captcha" id="id_11">
-                    <label class="form-label form-label-top form-label-auto" id="label_11" for="input_11" aria-hidden="false"> Please verify that you are human<span class="form-required">*</span>
-                 </label>
+                    <label class="form-label form-label-top form-label-auto" id="label_11" for="input_11" aria-hidden="false"> Please verify that you are human<span class="form-required">*</span></label>
 
                     <div id="cid_11" class="form-input-wide jf-required" data-layout="full">
                         <section data-wrapper-react="true">
-                            <div id="hcaptcha_input_11" class="h-captcha" data-siteKey="772f4a50-7161-425e-8cd5-4d7e361ab765" data-callback="hcaptchaCallbackinput_11" data-expired-callback="hcaptchaExpiredCallbackinput_11"></div><input type="hidden" id="input_11" class="hidden validate[required]" name="hcaptcha_visible" required />
-                            <script type="text/javascript" src="https://hcaptcha.com/1/api.js"></script>
-                            <script type="text/javascript">
-                                var hcaptchaCallbackinput_11 = function(token) {
-                                    var hiddenInput = $("input_11");
-                                    hiddenInput.setValue(1);
-                                    if (hiddenInput.validateInput) {
-                                        hiddenInput.validateInput();
-                                    }
-                                }
-
-                                var hcaptchaExpiredCallbackinput_11 = function() {
-                                    var hiddenInput = $("input_11");
-                                    hiddenInput.writeAttribute("value", false);
-                                    if (hiddenInput.validateInput) {
-                                        hiddenInput.validateInput();
-                                    }
-                                }
-                            </script>
+                            <div id="hcaptcha_input_11" class="h-captcha"
+                                data-sitekey="772f4a50-7161-425e-8cd5-4d7e361ab765"
+                                data-callback="onCaptchaSuccess"
+                                data-expired-callback="onCaptchaExpired"></div>
+                            <input type="hidden" id="input_11" class="hidden validate[required]" name="hcaptcha_visible" required />
+                            <script type="text/javascript" src="https://hcaptcha.com/1/api.js" async defer></script>
                         </section>
                     </div>
-                            </div>
-                <div class="contact-form-btn">
-
-                    <button type="submit" class="theme-btn">Submit</button>
-
                 </div>
 
+                <div class="contact-form-btn">
+                    <button type="submit" class="theme-btn" id="submitButton" disabled>Submit</button>
+                </div>
             </form>
+
+            <script>
+                const submitButton = document.getElementById('submitButton');
+
+                // Callback function when hCaptcha is successfully completed
+                function onCaptchaSuccess(token) {
+                    document.getElementById('input_11').value = token; // Set the hidden input value
+                    submitButton.disabled = false; // Enable the submit button
+                }
+
+                // Callback function when hCaptcha expires
+                function onCaptchaExpired() {
+                    document.getElementById('input_11').value = ''; // Clear the hidden input value
+                    submitButton.disabled = true; // Disable the submit button
+                }
+            </script>
+
 
         </div>
 
     </div>
 
 </section>
-
-
-
 
 
 <?php
