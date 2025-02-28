e2f9f778cd05ba291db2a0de455550d1c2abf896
aasimdev <aasimdev69@gmail.com>
2025-02-28 22:48:38 +0500

    updated explainer page

--- pages/explainer.php
+++ pages/explainer.php
@@ -1,7 +1,7 @@
 <section class=" xl:pt-[100px] pt-20">
     <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 ">
         <h1 class='max-w-[950px] mx-auto w-full text-[50px] text-center'>
-        Explainer Video
+        Short Explainer Video
         </h1>
         <div class="mt-[89px]">
             <iframe class='w-full aspect-video' src="https://www.youtube.com/embed/TqdrYBnj8gY"
