2fdd22bdc19ce3184d521dbf935c64f417647cc1
aasimdev <aasimdev69@gmail.com>
2025-04-14 21:09:15 +0500

    Add new podcast video for April 14, 2025

--- data/podcast-data.php
+++ data/podcast-data.php
@@ -1,6 +1,14 @@
 <?php
 $videos = [
     [
+        'title' => "April 14, 2025 - NewHydrogen News Commentary",
+        'videoID' => "YyZ8CorWGnc",
+        'date' => "",
+        'category' => "news-commentary",
+        'slug' => "april-14-2025-newhydrogen-news-commentary",
+        "display" => true
+    ],
+    [
         'title' => "April 7, 2025 - NewHydrogen News Commentary",
         'videoID' => "6gItpIlY78Y",
         'date' => "",
