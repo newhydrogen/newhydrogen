1cafec52e6ff13db80f3920bd5e58f6fa34e4f3a
aasimdev <aasimdev69@gmail.com>
2025-04-21 02:29:39 +0500

    Remove unused CSS classes and update valid pages in router to include 'fasttrack'.

---
+++ pages/fasttrack.php
@@ -0,0 +1,12 @@
+<section class="xl:pt-[100px] pt-20 pb-[135px]">
+    <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4">
+        <h1 class="w-full text-[50px] text-center font-semibold">We Are Moving Very Rapidly on a Fast Track</h1>
+        <p class="text-center text-3xl mt-6 text-custom-orange-100 font-semibold">Click this short video to learn more about our progress</p>
+        <div class="mt-16">
+            <iframe width="560" height="315" class="h-64 sm:h-[600px] w-full" src="https://www.youtube.com/embed/1231123123"
+                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+                            encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+            </iframe>
+        </div>
+    </div>
+</section>
