2adf630e466b38eaefdf824789b63362828e6e4a
aasimdev <aasimdev69@gmail.com>
2025-06-25 02:36:58 +0500

    Refactor team and about pages to improve layout consistency and responsiveness; update CSS for margin adjustments and new utility classes; enhance team member presentation with additional spacing for better readability.

--- pages/about.php
+++ pages/about.php
@@ -39,16 +39,16 @@
     </div>
 </section>
 
-<section class="bg-custom-gray-400">
+<section>
     <div class="mx-auto px-2 sm:px-4 lg:max-w-screen-lg xl:max-w-screen-xl py-20">
         <div>
             <h2 class="text-[36px] lg:text-5xl font-black leading-[1.1]">Our Company Team</h2>
             <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-[120px] gap-y-10 mt-16">
-                <div class="bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                <div class="bg-white rounded-[30px] p-6 h-auto relative max-w-[calc(100%_-_75px)] lg:max-w-full">
+                    <div class="flex flex-col gap-6">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>David Lee, PhD</h6>
-                            <p class="text-sm">Chairman of the Board & President</p>
+                            <p class="text-sm mt-1.5">Chairman of the Board & President</p>
                         </div>
                         <p class="pr-20 text-sm">35 years of experience in sustainable energy, software development, electronics, engineering, marketing, sales, and corporate management.</p>
                     </div>
@@ -56,11 +56,11 @@
                         <img src="./assets/images/team-member5.png" alt="team-member" class="rounded-full w-full h-full overflow-hidden">
                     </div>
                 </div>
-                <div class="bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                <div class="bg-white rounded-[30px] p-6 h-auto relative max-w-[calc(100%_-_75px)] lg:max-w-full">
+                <div class="flex flex-col gap-6">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>Steve Hill, MBA</h6>
-                            <p class="text-sm">Chief Executive Officer</p>
+                            <p class="text-sm mt-1.5">Chief Executive Officer</p>
                         </div>
                         <p class="pr-20 text-sm">Over 20 years of experience in biopharmaceutical and tech business development, sustainable energy, product launches, and market strategy.</p>
                     </div>
@@ -68,11 +68,11 @@
                         <img src="./assets/images/team-member6.png" alt="team-member" class="rounded-full w-full h-full overflow-hidden">
                     </div>
                 </div>
-                <div class="bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                <div class="bg-white rounded-[30px] p-6 h-auto relative max-w-[calc(100%_-_75px)] lg:max-w-full">
+                    <div class="flex flex-col gap-6 h-full">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>Eric McFarland, PhD</h6>
-                            <p class="text-sm">Chief Technology Officer</p>
+                            <p class="text-sm mt-1.5">Chief Technology Officer</p>
                         </div>
                         <p class="pr-20 text-sm">UCSB professor, inventor, and energy technology expert with deep experience in catalysis, hydrogen production, and industry-academic collaboration. Former founder and CEO of multiple startups, with over 190 publications and 25 patents.</p>
                     </div>
@@ -80,11 +80,11 @@
                         <img src="./assets/images/team-member2.png" alt="team-member" class="rounded-full w-full h-full overflow-hidden">
                     </div>
                 </div>
-                <div class="bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                <div class="bg-white rounded-[30px] p-6 h-auto relative max-w-[calc(100%_-_75px)] lg:max-w-full">
+                    <div class="flex flex-col gap-6">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>Sundar Narayanan</h6>
-                            <p class="text-sm">Director of Process Engineering</p>
+                            <p class="text-sm mt-1.5">Director of Process Engineering</p>
                         </div>
                         <p class="pr-20 text-sm">Chemical process engineering and scale-up expert with over 35 years of experience in process development, technology integration, and commercialization. former ExxonMobil senior technologist with a track record in energy efficiency, process automation, and advanced systems.</p>
                     </div>
@@ -92,11 +92,11 @@
                         <img src="./assets/images/sundar-narayanan.png" alt="team-member" class="rounded-full w-full h-full overflow-hidden">
                     </div>
                 </div>
-                <div class="bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                <div class="bg-white rounded-[30px] p-6 h-auto relative max-w-[calc(100%_-_75px)] lg:max-w-full">
+                    <div class="flex flex-col gap-6">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>Ivor John, PhD</h6>
-                            <p class="text-sm">Senior Advisor</p>
+                            <p class="text-sm mt-1.5">Senior Advisor</p>
                         </div>
                         <p class="pr-20 text-sm">Environmental consultant, auditor and Ph.D. in Atmospheric Physics. Expert in air quality, climate change and the analysis of air emissions and greenhouse gasses.</p>
                     </div>
@@ -104,11 +104,11 @@
                         <img src="./assets/images/ivor-john.png" alt="team-member" class="rounded-full w-full h-full overflow-hidden">
                     </div>
                 </div>
-                    <div class="bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                    <div class="bg-white rounded-[30px] p-6 h-auto relative max-w-[calc(100%_-_75px)] lg:max-w-full">
+                    <div class="flex flex-col gap-6">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>Nirala Singh, PhD</h6>
-                            <p class="text-sm">Scientific Advisor</p>
+                            <p class="text-sm mt-1.5">Scientific Advisor</p>
                         </div>
                         <p class="pr-20 text-sm">Expert in Electrocatalysts Development Associate Professor of Chemical Engineering, University of Michigan, Ann Arbor.</p>
                     </div>
@@ -121,11 +121,11 @@
         <div class="mt-28">
             <h2 class="text-[36px] lg:text-5xl font-black leading-[1.1]">The UCSB Technology Team</h2>
             <div class="grid grid-cols-1 md:grid-cols-12 gap-x-[120px] gap-y-10 mt-16">
-                <div class="md:col-span-4 xl:col-start-5 bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                <div class="md:col-span-4 xl:col-start-5 bg-white rounded-[30px] p-6 h-auto relative max-w-[calc(100%_-_75px)] lg:max-w-full">
+                    <div class="flex flex-col gap-6">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>Phillip Christopher, PhD</h6>
-                            <p class="text-sm">Principal Investigator</p>
+                            <p class="text-sm mt-1.5">Principal Investigator</p>
                         </div>
                         <p class="pr-20 text-sm">Associate Professor of Chemical Engineering, University of California, Santa Barbara</p>
                     </div>
@@ -137,10 +137,10 @@
             <div class="grid grid-cols-1 md:grid-cols-12 gap-x-[120px] gap-y-10 mt-16">
               
                 <div class="md:col-span-4 xl:col-start-3 bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                    <div class="flex flex-col gap-6">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>Justin Marlowe, PhD </h6>
-                            <p class="text-sm">Research Scientist</p>
+                            <p class="text-sm mt-1.5">Research Scientist</p>
                         </div>
                         <p class="pr-20 text-sm">Ph.D. in Chemical Engineering University of California, Santa Barbara. Expert in multiple materials characterization techniques and reaction engineering.</p>
                     </div>
@@ -148,11 +148,11 @@
                         <img src="./assets/images/team-member3.png" alt="team-member" class="rounded-full w-full h-full overflow-hidden">
                     </div>
                 </div>
-                <div class="md:col-span-4 bg-white rounded-[30px] p-6 h-auto md:h-[300px] relative max-w-[calc(100%_-_75px)] lg:max-w-full">
-                    <div class="flex flex-col justify-between h-full">
+                <div class="md:col-span-4 bg-white rounded-[30px] p-6 h-auto relative max-w-[calc(100%_-_75px)] lg:max-w-full">
+                    <div class="flex flex-col gap-6">
                         <div class="relative before:content-[''] before:absolute before:top-0 before:-left-6 before:w-0.5 before:h-full before:bg-[#76b3f8] before:bg-no-repeat md:mb-0 mb-4 md:pr-0 pr-16">
                             <h6>Yikyeom Kim, PhD </h6>
-                            <p class="text-sm">Research Scientist</p>
+                            <p class="text-sm mt-1.5">Research Scientist</p>
                         </div>
                         <p class="pr-20 text-sm">Ph.D. in Chemical Engineering from the Korea Advanced Institute of Science and Technology (KAIST). Expert in the fabrication of tailored redox catalysts and system analysis of solid-gas reactions.</p>
                     </div>
