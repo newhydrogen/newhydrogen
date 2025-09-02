cf0760cb2acbe7d1780cee08ed01fe97caeb15e0
aasimdev <aasimdev69@gmail.com>
2025-09-03 01:53:28 +0500

    Add new video for September 2, 2025, titled "NewHydrogen News Commentary," including video ID, category, slug, and display status in both carousel and podcast data to enhance content offerings in the news-commentary category.

--- data/podcast-data.php
+++ data/podcast-data.php
@@ -1,6 +1,14 @@
 <?php
 $videos = [
     [
+        'title' => "September 2, 2025 - NewHydrogen News Commentary",
+        'videoID' => "hPgghfPDq4M",
+        'date' => "",
+        'category' => "news-commentary",
+        'slug' => "september-2-2025-newhydrogen-news-commentary",
+        "display" => true
+    ],
+    [
         'title' => "August 25, 2025 - NewHydrogen News Commentary",
         'videoID' => "tuzxOPue-Co",
         'date' => "",
