8e06fe20b498dcedaa0ac57cc2f27b74642e4133
aasimdev <aasimdev69@gmail.com>
2025-10-14 02:23:32 +0500

    Replace "Coming Soon" section in home.php with a new banner layout, enhancing visual appeal and user engagement. The previous content and registration form have been removed to streamline the design.

--- pages/home.php
+++ pages/home.php
@@ -17,22 +17,7 @@
             </div>
     </div>
 </section> -->
-<section class="bg-coming-soon-new bg-cover bg-center bg-no-repeat bg-black relative py-32">
-    <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative">
-        <div class="text-6xl text-white text-center font-bold">Coming Soon</div>
-        <div class="text-white text-4xl text-center font-semibold my-4 leading-snug">NewHydrogen to Reveal its Massive <br class="hidden md:inline-block" />
-        Source of Heat for ThermoLoop
-        </div>
-        <p class="text-2xl text-center text-white font-medium">Reserve your spot now for our upcoming webinar ######, <br class="hidden md:inline-block" /> October######, 2025, at 4pm EST (1pm PST)
-        </p>
-        <form action="https://submit.jotform.com/submit/250137276726055/" method="POST" autocomplete="off" class="relative max-w-[420px] mx-auto mt-8">
-            <input type="hidden" name="formID" value="250137276726055">
-            <input type="email" id="webinar-email" name="q3_webinaremail" placeholder="Enter email address" required class="w-full h-12 rounded-xl py-1 pl-4 pr-32 text-base border-0 focus-visible:outline-none">
-         
-            <button type="submit" id="webinarSubmitBtn" class="bg-green-500 text-black py-2.5 px-6 rounded-xl text-base font-semibold absolute right-0 top-0 h-12">Sign up!</button>
-        </form>
-    </div>
-</section>
+
 <section class="bg-banner bg-cover bg-[18%_100%] md:bg-center relative md:pb-36 md:pt-28 py-16 ">
     <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative">
         <div class="md:grid grid-cols-12 block">
