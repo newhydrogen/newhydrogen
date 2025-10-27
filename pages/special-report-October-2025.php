6a9f5b84f3e5a8521697e73c4f233937e245a439
aasimdev <aasimdev69@gmail.com>
2025-10-27 10:53:29 +0500

    Refactor CSS utility classes for margin, gap, and line-height, while updating the special report page layout for improved responsiveness and visual hierarchy. Adjust font sizes and margins for better readability across devices.

--- pages/special-report-October-2025.php
+++ pages/special-report-October-2025.php
@@ -1,8 +1,8 @@
 
 
-<div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative py-40">
-        <div class="text-6xl text-center font-bold">NewHydrogen Special Report - October 27, 2025</div>
-        <div class="lg:max-w-[995px] max-w-[830px] mx-auto relative youtube-video-wrapper mt-14" data-video-id="xsT6_tSNjUs">
+<div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 z-10 relative sm:py-40 py-20">
+        <div class="sm:text-5xl text-3xl md:text-6xl text-center font-bold">NewHydrogen Special Report - October 27, 2025</div>
+        <div class="lg:max-w-[995px] max-w-[830px] mx-auto relative youtube-video-wrapper sm:mt-14 mt-8" data-video-id="xsT6_tSNjUs">
         <iframe id="youtube-player" src="https://www.youtube.com/embed/xsT6_tSNjUs?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
         <button type="button" class="absolute inset-0 flex items-center justify-center cursor-pointer z-20 youtube-custom-btn">
             <svg class="sm:w-[100px] sm:h-[100px] w-16 h-16" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 56 56" fill="none">
