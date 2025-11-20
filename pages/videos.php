7e4a7b592a6afeffb8d51910e141a68482110c5a
aasimdev <aasimdev69@gmail.com>
2025-11-21 02:58:07 +0500

    Refactor CSS styles by removing unused classes and updating margin and padding values for better consistency. Add a new video section in videos.php with a YouTube embed for enhanced content delivery.

--- pages/videos.php
+++ pages/videos.php
@@ -7,7 +7,7 @@ $videos = include "./data/podcast-data.php";
 
 ?>
 
-<section class=" xl:pt-[100px] pt-20 pb-[135px]">
+<section class=" xl:pt-[100px] pt-20 sm:pb-[135px]">
     <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4 ">
  
 
@@ -149,6 +149,20 @@ $videos = include "./data/podcast-data.php";
 
         </section>
         <section class="py-[90px]">
+        <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl px-2 sm:px-4">
+        <h4 class="font-normal pt-[13px] xl:text-[64px] text-5xl text-center">Nuclear Power</h4>
+    <div class="max-w-full mx-auto relative youtube-video-wrapper py-10 sm:mt-14" data-video-id="adEZEqdqHYk">
+        <iframe id="youtube-player" src="https://www.youtube.com/embed/adEZEqdqHYk?enablejsapi=1&controls=0&playsinline=1&rel=0&modestbranding=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen class="w-full aspect-video relative z-10"></iframe>
+        <button type="button" class="absolute inset-0 flex items-center justify-center cursor-pointer z-20 youtube-custom-btn">
+            <svg class="sm:w-[100px] sm:h-[100px] w-16 h-16" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 56 56" fill="none">
+                <circle cx="27.8926" cy="28.1125" r="27.5" fill="white" fill-opacity="1"></circle>
+                <path d="M39.1753 28.1126L21.5537 38.2864L21.5537 17.9387L39.1753 28.1126Z" fill="black"></path>
+            </svg>
+        </button>
+    </div>
+    </div>
+        </section>
+        <!-- <section class="py-[90px]">
             <div class="flex justify-between items-baseline pb-[37px]   mb-[58px] border-b border-[#cbcbcb]"
                 id='short-video'>
                 <h3 class=" xl:text-[64px] md:text-5xl text-3xl font-extralight leading-[1.1]">Short Videos</h3>
@@ -194,6 +208,6 @@ $videos = include "./data/podcast-data.php";
                 }
                 ?>
             </div>
-        </section>
+        </section> -->
     </div>
 </section>
